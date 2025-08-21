0e70546e38a3ee8d0f5e371fe1464249ded64bfc
Darwin Panchez <djpanchez@espe.edu.ec>
2025-08-21 10:47:53 -0500

    Ok

--- backend/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-// environments/environment.js
-module.exports = {
-  environment: {
-    production: true,
-    apiUrl: 'https://tienda-sneakers-online.onrender.com'
-  }
-};
--- backend/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// environments/environment.js
-module.exports = {
-  environment: {
-    production: false,
-    apiUrl: 'http://localhost:3000'
-  }
-};
